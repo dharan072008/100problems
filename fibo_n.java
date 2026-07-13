4260d0014ed888c0af8d402d6f3c3ac4121121af
Dharan <dharanjs07@gmail.com>
2026-07-13 21:33:48 +0530

    the updated fibonacci series

--- fibo_n.java
+++ fibo_n.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 class fibo_n{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the number of terms: ");
+        System.out.print("Enter the number of terms to be printed: ");
         int n = scan.nextInt();
 
         int a = 0, b = 1;
 
-        System.out.println("Fibonacci Series:");
+        System.out.println("The Fibonacci Series is : ");
 
         for (int i = 1; i <= n; i++) {
             System.out.print(a + " , ");
